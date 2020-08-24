15a6238ec99dba49d71e59b199cc680783c5d4ef
Nur Muhammad "Mahi" Shafiullah <n.m.mahi@gmail.com>
2020-08-24 14:00:03 -0400

    Updated map.js to use daily updated link

--- js/map.js
+++ js/map.js
@@ -13,9 +13,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var googleSheetsUrl = "https://notmahi.github.io/bd-rt-dashboard/static/rt_bd_june_7_web.csv";
 
-const Rt_url = "https://notmahi.github.io/bd-rt-dashboard/static/rt_bangladesh.json";
+const Rt_url = "https://cs.nyu.edu/~nms572/covid/latest/rt_bangladesh.json" + Date.now();
 const popUrl = "https://notmahi.github.io/bd-rt-dashboard/static/bd_population.json";
-const caseHistoryUrl = "https://notmahi.github.io/bd-rt-dashboard/static/bd_case_history.json";
+const caseHistoryUrl = "https://cs.nyu.edu/~nms572/covid/latest/bd_case_history.json" + Date.now();
 
 function handle_rt_data(response) {
     const rtData = response;
